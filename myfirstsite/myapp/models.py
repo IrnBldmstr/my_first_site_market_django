1ec4acabf5be183955cc4cf21a926a56c3a26f59
damir_kozhakan <oleg.tangaldiev@gmail.com>
2024-02-26 16:19:38 +0600

    detail styled html

--- myfirstsite/myapp/models.py
+++ myfirstsite/myapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
-    discription = models.CharField(max_length=1000)
-    image = models.ImageField(blank=True, upload_to= 'images')
+    description = models.CharField(max_length=1000)
+    image = models.ImageField(blank=True, upload_to='images')
 
 
     def __str__(self):
