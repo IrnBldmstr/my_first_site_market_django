02ab1dcee2db0a9959442896e54fb9cc0e179ea2
damir_kozhakan <oleg.tangaldiev@gmail.com>
2024-02-26 17:48:27 +0600

    add product + button

--- myfirstsite/myapp/urls.py
+++ myfirstsite/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from myapp.views import index, indexItem
+from myapp.views import index, indexItem, add_item
 
 app_name = 'myapp'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     # http://127.0.0.1:8000/myapp/
     path('', index),
     path('<int:my_id>/', indexItem, name='detail'),
+    path('additem/', add_item, name='add_item'),
 ]
