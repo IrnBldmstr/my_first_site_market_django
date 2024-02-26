fa476c29deff5e326978e389d5335999a6c2902f
damir_kozhakan <oleg.tangaldiev@gmail.com>
2024-02-26 10:36:50 +0600

    added image in models

--- myfirstsite/myapp/models.py
+++ myfirstsite/myapp/models.py
@@ -4,5 +4,8 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     discription = models.CharField(max_length=1000)
+    image = models.ImageField(blank=True, upload_to= 'images')
+
+
     def __str__(self):
         return self.name
